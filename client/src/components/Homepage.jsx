87115d3d2196dc26dbb39bec16100ea6155bbd0e
ccantu20 <carloscq920@gmail.com>
2021-10-04 17:49:23 -0500

    Added mock data to the homepage of the site using Ant Design

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Typography.Title level={2} className="heading">
+                Global Crypto Stats
+            </Typography.Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title="Total Crypto Currencies" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchanges" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Marketcap" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24 hour volume" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value="5"/>
+                </Col>  
+            </Row>
+        </>
     )
 }
 
