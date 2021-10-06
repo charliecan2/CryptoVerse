629ae67753a2311819969d8abf7f8687da35ff70
ccantu20 <carloscq920@gmail.com>
2021-10-05 21:29:44 -0500

    Fetched the proper data from the stats endpoint from the Coinranking API, converted it to a readable string using millify, and displayed it on the homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
+import 'millify';
 import { Link } from 'react-router-dom';
-
+import millify from 'millify';
+ 
 import { useGetCryptosQuery } from '../services/cryptoAPI';
 
 const Homepage = () => {
@@ -11,6 +12,10 @@ const Homepage = () => {
 
     console.log(data);
 
+    const globalStats = data?.data;
+
+    if(isFetching) return 'Loading...';
+ 
     return (
         <>
             <Typography.Title level={2} className="heading">
@@ -18,20 +23,20 @@ const Homepage = () => {
             </Typography.Title>
             <Row>
                 <Col span={12}>
-                    <Statistic title="Total Crypto Currencies" value="5"/>
+                    <Statistic title="Total Cryptocurrencies" value={globalStats.totalCoins}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Exchanges" value="5"/>
+                    <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Marketcap" value="5"/>
+                    <Statistic title="Total MarketCap" value={millify(globalStats.totalMarketCap)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total 24 hour volume" value="5"/>
+                    <Statistic title="Total 24 hour volume" value={millify(globalStats.total24hVolume)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Markets" value="5"/>
-                </Col>  
+                    <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
+                </Col>
             </Row>
         </>
     )
