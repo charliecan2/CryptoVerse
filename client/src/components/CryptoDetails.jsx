7a8c3374125c20f0562b364ead37a9586bf039ae
ccantu20 <carloscq920@gmail.com>
2021-10-11 11:43:07 -0500

    Successfully build a new query in cryptoAPI.js to retrieve more data on a specific cryptocurrency. Successfully fetched the data, is only logged for now

--- client/src/components/CryptoDetails.jsx
+++ client/src/components/CryptoDetails.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import parse from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+import { useGetCryptoDetailsQuery } from '../services/cryptoAPI';
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
+    const { coinId } = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d');
+    const { data: crypto, isFetching } = useGetCryptoDetailsQuery(coinId);
+
+    console.log(crypto);
+
     return (
         <div>
-            CryptoDetails
+            CryptoDetails { coinId }
         </div>
     )
 }
